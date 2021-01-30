aba2cf5d898b61df656478ec2b52555016b68768
Mahmudul Hasan Sagar <mahmudsagar9@gmail.com>
2021-01-31 01:32:40 +0600

    final design

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+// const btnHamburger = document.querySelector("#btnHamburger");
+// const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  // console.log("open hamburger");
+
+  if (header.classList.contains("open")) {
+    //close hamburger menu
+    // body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // hamburger menu
+    // body.classList.add('noscroll');
+    header.classList.add('open');
+    fadElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
