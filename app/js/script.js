77ae14d8ac4c05fc860356087239ebd208c0ec62
Mahmaudul Hasan Sagar <mahmud@DESKTOP-ACERHRA.localdomain>
2021-02-19 22:04:54 +0600

    added sticky top

--- app/js/script.js
+++ app/js/script.js
@@ -25,3 +25,16 @@ btnHamburger.addEventListener("click", function () {
     });
   }
 });
+
+// sticky top
+const navbar = document.querySelector("nav")
+const sticky = navbar.offsetTop;
+window.onscroll = () =>{
+  console.log('sticky')
+  if(window.pageYOffset > sticky){
+    navbar.classList.add("sticky")
+  }
+  else{
+    navbar.classList.remove("sticky")
+  }
+}
